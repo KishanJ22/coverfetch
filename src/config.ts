10783964d6168a9ba00a95c1297b7ed6d3e5eed0
Kishan <kishanjethwa224@gmail.com>
2026-09-25 18:54:44 +0100

    update config to include ai api key and flipt config

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,16 @@ export const configSchema = z.object({
 		baseUrl: z.httpUrl(),
 		apiKey: z.string(),
 	}),
+	ai: z.object({
+		anthropicApiKey: z.string(),
+	}),
+	flipt: z.object({
+		baseUrl: z.httpUrl(),
+		authUrl: z.httpUrl(),
+		authClientId: z.string(),
+		authUsername: z.string(),
+		authPassword: z.string(),
+	}),
 });
 
 export type Config = z.infer<typeof configSchema>;
@@ -19,5 +29,15 @@ export const config: z.input<typeof configSchema> = {
 	hardcover: {
 		baseUrl: Bun.env.HARDCOVER_BASE_URL,
 		apiKey: Bun.env.HARDCOVER_API_KEY,
+	},
+	ai: {
+		anthropicApiKey: Bun.env.ANTHROPIC_API_KEY,
+	},
+	flipt: {
+		baseUrl: Bun.env.FLIPT_BASE_URL,
+		authUrl: Bun.env.AUTH_URL,
+		authClientId: Bun.env.AUTH_CLIENT_ID,
+		authUsername: Bun.env.AUTH_USERNAME,
+		authPassword: Bun.env.AUTH_PASSWORD,
 	},
 };
--- src/types/bun.d.ts
+++ src/types/bun.d.ts
@@ -1,7 +1,13 @@
 declare module "bun" {
 	interface Env {
-		SERVER_PORT: number;
+		AUTH_URL: string;
+		AUTH_CLIENT_ID: string;
+		AUTH_USERNAME: string;
+		AUTH_PASSWORD: string;
+		ANTHROPIC_API_KEY: string;
 		HARDCOVER_BASE_URL: string;
 		HARDCOVER_API_KEY: string;
+		FLIPT_BASE_URL: string;
+		SERVER_PORT: number;
 	}
 }
